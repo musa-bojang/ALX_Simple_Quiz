e26b6cf34706a23fb58d39daf4514950f9a026e0
musa-bojang <musabojang413@gmail.com>
2025-08-03 09:56:25 +0100

    Implement a Simple Interactive Quiz

--- quiz.js
+++ quiz.js
@@ -1,13 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
     function checkAnswer() {
         const correctAnswer = '4';
+        const feedback = document.getElementById('feedback');
         const selectedAnswer = document.querySelector('input[name="quiz"]:checked');
         const userAnswer = selectedAnswer.value;
-        if (userAnswer) {
-            console.log("Selected radio element:", userAnswer);
-            console.log("User's answer:", userAnswer.value);
-
-            const feedback = document.getElementById('feedback');
+      
+            
             if(userAnswer === correctAnswer) {
                 feedback.textContent = "Correct! Well done.";
                 feedback.style.color = "#28a745"; // Green for correct answer
@@ -15,11 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 feedback.textContent = "That's incorrect. Try again!";
                 feedback.style.color = "#dc3545"; // Red for incorrect answer
             }
-        } else {
-            console.log("No option selected.");
-            feedback.textContent = "Please select an answer before submitting.";
-            feedback.style.color = "#dc3545"; // Red for no selection
-        }
+        
 
 
         
